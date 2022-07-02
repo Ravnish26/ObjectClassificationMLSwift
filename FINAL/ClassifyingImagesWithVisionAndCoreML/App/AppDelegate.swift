fd94a05426c893de9bb1bcec8ccbffc89e990460
RavnishSignh_26 <65758722+Ravnish26@users.noreply.github.com>
2022-07-02 11:04:08 +0530

    Add files via upload

---
+++ FINAL/ClassifyingImagesWithVisionAndCoreML/App/AppDelegate.swift
@@ -0,0 +1,11 @@
+//  AppDelegate
+//
+//  Created by Ravnish Singh on 01/05/22.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
